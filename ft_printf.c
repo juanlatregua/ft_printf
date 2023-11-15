c1cf0dd88d7181067f1f895c7854913968ae5171
jsilva-m <juanlatregua@gmail.com>
2023-11-15 14:11:24 +0100

    fallo hex

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: jsilva-m <jsilva-m@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 11:49:26 by jsilva-m          #+#    #+#             */
-/*   Updated: 2023/11/12 15:50:41 by jsilva-m         ###   ########.fr       */
+/*   Updated: 2023/11/13 10:14:58 by jsilva-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printchar(int c)
+int	ft_printchar(char c)
 {
 	write(1, &c, 1);
 	return (1);
@@ -20,10 +20,9 @@ int	ft_printchar(int c)
 
 int	ft_formats(va_list args, const char format)
 {
-	int	len;;
-	
+	int		len;
 	char	*hex;
-	
+
 	len = 0;
 	hex = "0123456789abcdef";
 	if (format == 'c')
@@ -33,12 +32,12 @@ int	ft_formats(va_list args, const char format)
 	else if (format == 'p')
 	{
 		len += ft_printstr("0x");
-		len += (va_arg(args, unsigned	long), hex);
+		len += (va_arg(args, unsigned long), hex);
 	}
 	else if (format == 'd' || format == 'i')
 		len += ft_printnbr(va_arg(args, int));
 	else if (format == 'u')
-		len += ft_print_unsigned(va_arg(args, unsigned int));
+		len += ft_print_unsig(va_arg(args, unsigned int));
 	else if (format == 'x' || format == 'X')
 		len += ft_print_hex(va_arg(args, unsigned int), format);
 	else if (format == '%')
@@ -49,7 +48,7 @@ int	ft_formats(va_list args, const char format)
 int	ft_printf(const char *str, ...)
 {
 	int		i;
-	va_list		args;
+	va_list	args;
 	int		len;
 
 	i = 0;
@@ -59,10 +58,12 @@ int	ft_printf(const char *str, ...)
 	{
 		if (str[i] == '%')
 			len += ft_formats(&args, str[i + 1]);
-			i++;
-		else
-			len += ft_putchar(str[i]);
 		i++;
+		else
+		{
+			len += ft_putchar(str[i]);
+			i++;
+		}
 	}
 	va_end(args);
 	return (len);
