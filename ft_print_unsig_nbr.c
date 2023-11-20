2cabbc8575e36185a40cd62e96c0703a007b9389
jsilva-m <jsilva-m@student.42malaga.com>
2023-11-20 18:43:20 +0100

    lunes20 modificaciones en print_unsing_nbr por los problemas n>=10 y la recursividad

--- ft_print_unsig_nbr.c
+++ ft_print_unsig_nbr.c
@@ -6,7 +6,7 @@
 /*   By: jsilva-m <jsilva-m@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 15:44:50 by jsilva-m          #+#    #+#             */
-/*   Updated: 2023/11/20 09:54:27 by jsilva-m         ###   ########.fr       */
+/*   Updated: 2023/11/20 18:39:44 by jsilva-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,13 @@ int	ft_print_unsig_nbr(unsigned int n)
 	int	len;
 
 	len = 0;
-	if (n > 9)
+	if (n >= 10)
 	{
-		len += ft_printnbr(n / 10);
-		len += ft_printnbr(n % 10);
+		len += ft_print_unsig_nbr(n / 10);
+		len += ft_print_unsig_nbr(n % 10);
 	}
 	else
 	{
-		len++;
 		len += ft_printchar(n + 48);
 	}
 	return (len);
