8b115ff598862a740aa37b599cd27f8479c446d3
jsilva-m <juanlatregua@gmail.com>
2023-11-27 10:14:42 +0100

    quito el tabulador

--- ft_printchar.c
+++ ft_printchar.c
@@ -6,7 +6,7 @@
 /*   By: jsilva-m <jsilva-m@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 19:56:09 by jsilva-m          #+#    #+#             */
-/*   Updated: 2023/11/20 12:17:31 by jsilva-m         ###   ########.fr       */
+/*   Updated: 2023/11/27 10:13:56 by jsilva-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,6 @@
 
 int	ft_printchar(int c)
 {
-	if (c == '\t')
-	{
-		write(1, "\t", 1);
-		return (1);
-	}
-	else
-	{
-		write(1, &c, 1);
-		return (1);
-	}
+	write(1, &c, 1);
+	return (1);
 }
